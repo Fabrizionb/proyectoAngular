8f1b1bbb500995bed0286de1c84e928dfd7c756a
Fabrizionb <fabrizio1007@gmail.com>
2022-09-04 14:12:44 -0300

    todo el hmtl en modulos, creacion de servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { MasInfoComponent } from './componentes/mas-info/mas-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    AcercaComponent,
+    FooterComponent,
+    PortfolioComponent,
+    ExperienciaComponent,
+    AptitudesComponent,
+    MasInfoComponent
   ],
   imports: [
     BrowserModule,
